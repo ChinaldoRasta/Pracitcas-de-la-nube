7b13dbaed49bcc7ca83c47baa6b1bb35679a0e4b
AdrianRiveroll <adrianriceeoll981@gmail.com>
2026-01-25 09:52:33 -0600

    Creacion de la tabla editoriales

--- biblioteca/bd.sql
+++ biblioteca/bd.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE IF NOT EXISTS biblioteca_crud
 USE biblioteca_crud;
+
+-- Catálogo de editoriales
+CREATE TABLE IF NOT EXISTS editoriales (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(120) NOT NULL UNIQUE,
+  activo TINYINT(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB;
